0eee02b946e9116e813eae2b12e3a37c2573e67b
Emil <codewithemil@gmail.com>
2022-02-01 19:00:13 -0600

    I added a function where, if the background has less than a certain brightness, then our link's color will be either black or white.

--- JS/app backup.js
+++ JS/app backup.js
@@ -4,7 +4,7 @@ document.querySelector("#box").style.border = "2px solid #000";
 document.querySelector("a").style.color = "#000";
 
 //Color Picker Function Starts
-let backgroundColor = () => {
+backgroundColor = () => {
 
     //Previous styles are taken away
     document.querySelector("#box").style.border = "none";
@@ -28,26 +28,6 @@ let backgroundColor = () => {
 
     //Title tag's text is changed
     document.querySelector("#editableText").title = "Hex: " + hexColor;
-
-    let uwu = getBrightness(hexColor);
-
-    document.querySelector("a").style.color = uwu;
-}
-
-//Changing the text's color
-let getBrightness = color => {
-    color = color.replace("#", "");
-    var r = parseInt(color.substr(0,2),16);
-    var g = parseInt(color.substr(2,2),16);
-    var b = parseInt(color.substr(4,2),16);
-    var yiq = ((r*299)+(g*587)+(b*114))/1000;
-
-    /*console.log(yiq);
-    console.log(`From R, we have ${r}.`);
-    console.log(`From G, we have ${g}.`);
-    console.log(`From B, we have ${b}.`);*/
-
-    return (yiq >= 128) ? 'black' : 'white';
 }
 
 document.querySelector("button").addEventListener("click", backgroundColor);
